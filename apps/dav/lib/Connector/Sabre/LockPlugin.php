9793203eff5bdab12c53da27254a8b52ab55e7cd
Thomas Müller <thomas.mueller@tmit.eu>
2018-06-15 12:12:10 +0200

    Add verification step that only the lock owner can unlock

--- apps/dav/lib/Connector/Sabre/LockPlugin.php
+++ apps/dav/lib/Connector/Sabre/LockPlugin.php
@@ -24,10 +24,14 @@
 
 namespace OCA\DAV\Connector\Sabre;
 
+use OC\Lock\Persistent\LockMapper;
 use OCA\DAV\Connector\Sabre\Exception\FileLocked;
 use OCP\Lock\ILockingProvider;
 use OCP\Lock\LockedException;
+use Sabre\DAV\Exception\Forbidden;
 use Sabre\DAV\Exception\NotFound;
+use Sabre\DAV\Exception\NotImplemented;
+use Sabre\DAV\Locks\LockInfo;
 use Sabre\DAV\ServerPlugin;
 use Sabre\HTTP\RequestInterface;
 
@@ -46,6 +50,7 @@ class LockPlugin extends ServerPlugin {
 		$this->server = $server;
 		$this->server->on('beforeMethod', [$this, 'getLock'], 50);
 		$this->server->on('afterMethod', [$this, 'releaseLock'], 50);
+		$this->server->on('beforeUnlock', [$this, 'beforeUnlock'], 20);
 	}
 
 	public function getLock(RequestInterface $request) {
@@ -81,4 +86,19 @@ class LockPlugin extends ServerPlugin {
 			$node->releaseLock(ILockingProvider::LOCK_SHARED);
 		}
 	}
+
+	public function beforeUnlock($uri, LockInfo $lock) {
+		/** @var LockMapper $mapper */
+		$mapper = \OC::$server->query(LockMapper::class);
+
+		$lock = $mapper->getLockByToken($lock->token);
+		if ($lock === null || $lock->getAccountId() === null) {
+			return;
+		}
+		$currentUser = \OC::$server->getUserSession()->getUser();
+		if ($currentUser === null || $lock->getAccountId() !== $currentUser->getAccountId()) {
+			throw new Forbidden();
+		}
+	}
+
 }
